514c74b7f3c6c43a10d253ed4c1a32b3197084d0
Annisasafitri06 <safitriannisa153@gmail.com>
2024-02-26 11:32:58 +0700

    week1

---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+$nama ="Annisa Safitri";
+$jurusan ="Sistem Informasi";
+$semester = 2;
+
+echo 'Nama saya : ' .$nama;
+echo '</br> Jurusan : ' .$jurusan;
+echo '</br> Semester : ' .$semester;
+?>
